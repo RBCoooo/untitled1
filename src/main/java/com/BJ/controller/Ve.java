586c42c6807e7f7b313db8767d9cf6bc93927575
Layne <Layne@atgui.com>
2022-03-15 16:26:12 +0800

    hot-fix commit1

--- src/main/java/com/BJ/controller/Ve.java
+++ src/main/java/com/BJ/controller/Ve.java
@@ -4,6 +4,6 @@ public class Ve {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git");
-        System.out.println("hello Git33");
+        System.out.println("hello Git44");
     }
 }
